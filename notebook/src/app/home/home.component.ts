9a226fc76c2711d7497566492be9ac9271292357
Gurunathan <gurunathan.p@indiumsoft.com>
2025-09-12 23:44:22 +0530

    made page resizable

--- notebook/src/app/home/home.component.ts
+++ notebook/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, HostListener, ViewChild, viewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,31 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private cdr: ChangeDetectorRef) { }
 
+  @ViewChild('page') page!: ElementRef;
+  lineCount = 0;
+  lineCountRight = 0;
+  lineHeight = 36;
+
+  ngAfterViewInit() {
+    this.calculateLines();
+    this.cdr.detectChanges();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.calculateLines();
+  }
+
+  calculateLines() {
+    if (this.page) {
+      const height = this.page.nativeElement.offsetHeight;
+      this.lineCount = Math.floor(height / this.lineHeight);
+      this.lineCountRight = this.lineCount - 1; 
+      console.log('Height:', height, 'Line Count:', this.lineCount);
+    }
+  }
 }
+
+
